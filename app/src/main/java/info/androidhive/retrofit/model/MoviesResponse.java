fcbd8bcf50f0b0f107e1b649cea9c0f702935139
Michael Spinello <spinellomichael@yahoo.it>
2020-02-05 10:34:29 +0100

    commit branch

--- app/src/main/java/info/androidhive/retrofit/model/MoviesResponse.java
+++ app/src/main/java/info/androidhive/retrofit/model/MoviesResponse.java
@@ -3,7 +3,7 @@ package info.androidhive.retrofit.model;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
-//
+//modifica nel branch4
 
 public class MoviesResponse {
     @SerializedName("page")
